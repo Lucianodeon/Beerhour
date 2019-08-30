91677cd109fa37b491d029664d8e3449cfd630e0
lucianodeon <Lucianodeon@gmail.com>
2019-08-30 16:45:46 -0300

    hola

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>BEER HOUR</title>
+  </head>
+  <body>
+    <?php include "nav.php" ?>
+
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,11 @@
+<div class="container"> <!-- Contenedor ppal -->
+  <header> <!-- Encabezadp -->
+    <nav class="main-nav">
+      <a href="index.php"><h2 class="logo">BEER HOUR</h2></a>
+
+      <ul class="menu access">
+        <li><a href="login.php">Log in</a></li>
+        <li><a href="register.php">Registro</a></li>
+      </ul>
+    </nav>
+  </header>
