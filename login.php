3224c574c491a01ed4086e0ffc67ec6d00397f6f
lucianodeon <Lucianodeon@gmail.com>
2019-08-30 17:02:50 -0300

    .

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BEER HOUR| LOG IN</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <?php include "nav.php" ?>
+
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BEER HOUR | REGISTRATE</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <?php include "nav.php" ?>
+  </body>
+</html>
