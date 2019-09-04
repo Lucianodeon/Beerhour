bc9157a2c24b089a08920702cc0273d1f0af88a4
Matias <mataissoubelet@gmail.com>
2019-09-03 22:47:34 -0300

    Home y css
    Falta centrar los textos

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Chilanka|Lobster&display=swap" rel="stylesheet">
     <title>BEER HOUR</title>
   </head>
   <body>
     <?php include "nav.php" ?>
     <div class="beerhour">
-    <img src="img/logo.png" alt="logo_beerhor">
+    <img src="img/logo.png" alt="logo_beerhour">
     <h1>beer hour</h1>
     <h2>Don´t worry beer happy</h2>
     </div>
     <div class="text3">
-          <img src="img/_DSC00377.jpg" alt="foto1r">
-      <h2>¿Buscas variedad?</h2>
+          <img src="img/botellas-cerveza.png" alt="foto1r">
+      <h2>¿Queres variedad?</h2>
       <h3>¡Encuentra a los Brewers que se ajusten a tus gustos!</h2>
     </div>
     <div class="text3">
@@ -24,9 +25,9 @@
       <h3>¡Aqui te encontraran ellos primero!</h2>
     </div>
     <div class="text3">
-      <img src="img/_DSC00377.jpg" alt="fot3">
-      <h2>algo1</h2>
-      <h3>algo</h3>
+      <img src="img/mesa-vaso.png" alt="fot3">
+      <h2>¿Sos mayorista?</h2>
+      <h3>¡Este es tu lugar!</h3>
     </div>
   </body>
 </div>
