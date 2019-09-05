8f76cb06b98d949b4f14e635d6c4d8842789df83
Matias <mataissoubelet@gmail.com>
2019-09-04 21:32:28 -0300

    Todos los cambios de Css, index e imagenes
    Falta poco gente

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Chilanka|Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Anton|Chilanka|Lobster&display=swap" rel="stylesheet">
     <title>BEER HOUR</title>
   </head>
   <body>
@@ -12,10 +12,10 @@
     <div class="beerhour">
     <img src="img/logo.png" alt="logo_beerhour">
     <h1>beer hour</h1>
-    <h2>Don´t worry beer happy</h2>
+    <h2>Don´t Worry Beer Happy</h2>
     </div>
     <div class="text3">
-          <img src="img/botellas-cerveza.png" alt="foto1r">
+      <img src="img/botellas-cerveza.png" alt="foto1r">
       <h2>¿Queres variedad?</h2>
       <h3>¡Encuentra a los Brewers que se ajusten a tus gustos!</h2>
     </div>
@@ -28,6 +28,9 @@
       <img src="img/mesa-vaso.png" alt="fot3">
       <h2>¿Sos mayorista?</h2>
       <h3>¡Este es tu lugar!</h3>
+      <form class="botonhome" action="register.php" method="post">
+      <input class="button" type="submit" name="" value="¡Registrarse!">
+      </form>
     </div>
   </body>
 </div>
