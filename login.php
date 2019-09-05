25f3eee5bfc85988e1e756386f969e7479a2b1da
Matias <mataissoubelet@gmail.com>
2019-09-05 18:45:08 -0300

    Cambios Login y CSS
    Cambios Login y CSS

--- login.php
+++ login.php
@@ -4,29 +4,35 @@
     <meta charset="utf-8">
     <title>BEER HOUR| LOG IN</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet">
   </head>
   <body>
     <?php include "nav.php" ?>
     <main class="regi"> <!-- Cuerpo principal del sitio -->
       <section class="form-container">
         <h1>Bienvenido</h1>
-        <form class="access-form" action="" method="get">
+        <form class="access-form-log" action="" method="get">
           <div class="field-group">
-            <label for="email">Email</label>
+            <label class=logform for="email">Email</label>
+            <br>
             <input type="email" id="email" name="email" placeholder="Email" value="" required>
           </div>
+          <br>
           <div class="field-group">
-            <label for="password">Contraseña</label>
-            <input type="password" id="passsword" name="password" placeholder="password" value="" required>
+            <label class="logform" for="password">Contraseña</label>
+            <br>
+            <input type="password" id="passsword" name="password" placeholder="Contraseña" value="" required>
           </div>
+          <br>
           <div class="field-group remember-me">
             <input type="checkbox" id="remember-me" name="remember-me" value="">
-            <label for="remember-me">Recordarme</label>
+            <label class="logform" for="remember-me">Recordarme</label>
           </div>
           <p class="form-text alert-hide">
             La combinación ingresada de email y contraseña no es válida.
           </p>
-          <button type="submit" name="send">Ingresar</button>
+          <button class="botonlog" type="submit" name="send">Ingresar</button>
         </form>
       </section>
     </main>
